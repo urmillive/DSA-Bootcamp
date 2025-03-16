eec8642e8707dccb341daff466f149a23208744b
urmillive <urmillive@gmail.com>
2025-03-16 09:36:03 +0530

    refactor binary search function for improved readability and consistency

--- Arrays/binary-search.js
+++ Arrays/binary-search.js
@@ -1,21 +1,19 @@
-function search(arr,key){
-    let start=0;
-    let end = arr.length-1;
-    while(start<=end){
-        let mid = Math.floor(start + (end-start)/2)
-        if(arr[mid]==key){
-            return mid;
-        }
-        if(key>arr[mid]){
-            start = mid+1;
-        }
-        else{
-            end = mid-1;
-        }
-       
+function search(arr, key) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = Math.floor(start + (end - start) / 2);
+    if (arr[mid] == key) {
+      return mid;
     }
-    return 0;
+    if (key > arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return 0;
 }
 
-let arr = [3,5,9,13,27,55];
-console.log(search(arr,27));
+let arr = [3, 5, 9, 13, 27, 55];
+console.log(search(arr, 27));
